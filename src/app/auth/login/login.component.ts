6f5bf327ef99a115e60a062524535d1bf6900e2f
Zakariae Moutaouakil <moutaouakilzakariae.cpgea@gmail.com>
2024-02-09 23:57:28 +0100

    Added register logic

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,6 +1,11 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import {MatCard, MatCardContent} from "@angular/material/card";
 import {NgOptimizedImage} from "@angular/common";
+import {MatError, MatFormField, MatLabel} from "@angular/material/form-field";
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {MatInput} from "@angular/material/input";
+import {RouterLink} from "@angular/router";
+import {MatButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-login',
@@ -8,11 +13,39 @@ import {NgOptimizedImage} from "@angular/common";
   imports: [
     MatCard,
     MatCardContent,
-    NgOptimizedImage
+    NgOptimizedImage,
+    MatFormField,
+    ReactiveFormsModule,
+    MatInput,
+    MatLabel,
+    MatError,
+    RouterLink,
+    MatButton
   ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
+  encapsulation: ViewEncapsulation.None
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  signIn: FormGroup
+  emailFormControl: FormControl<string | null>;
+  passwordFormControl: FormControl<string | null>;
+  constructor() {
+    this.emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+    this.passwordFormControl = new FormControl('', [Validators.required, Validators.minLength(8)]);
+    this.signIn = new FormGroup<{ email: FormControl, password: FormControl }>({
+      'email': this.emailFormControl,
+      'password': this.passwordFormControl
+    })
+  }
 
+  ngOnInit(): void {
+    console.log(this.signIn)
+  }
+
+  OnSubmit() {
+    // console.log(this.signIn)
+    console.log("errors")
+    console.log(this.signIn.valid)
+  }
 }
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,12 +1,62 @@
-import { Component } from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
+import {MatCard} from "@angular/material/card";
+import {NgOptimizedImage} from "@angular/common";
+import {Router, RouterLink} from "@angular/router";
+import {MatError, MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {MatButton} from "@angular/material/button";
+import {HttpClient} from "@angular/common/http";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCard,
+    NgOptimizedImage,
+    RouterLink,
+    MatFormField,
+    MatInput,
+    MatLabel,
+    ReactiveFormsModule,
+    MatButton,
+    MatError
+  ],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrl: './register.component.scss',
+  encapsulation: ViewEncapsulation.None
 })
 export class RegisterComponent {
+  emailFormControl = new FormControl('', [Validators.required, Validators.email])
+  firstNameFormControl=new FormControl('', [Validators.required])
+  lastNameFormControl=new FormControl('', [Validators.required])
+  userNameFormControl=new FormControl('', [Validators.required])
+  passwordFormControl=new FormControl('', [Validators.required])
 
+
+  signUp = new FormGroup({
+    'firstname': this.firstNameFormControl,
+    'lastname': this.lastNameFormControl,
+    'email' : this.emailFormControl,
+    'username': this.userNameFormControl,
+    'password': this.passwordFormControl,
+  })
+
+  constructor(private readonly httpClient:HttpClient,
+              private readonly _snackBar: MatSnackBar,
+              private readonly router:Router) {
+  }
+
+  OnSubmit() {
+    console.log(this.signUp)
+    this.httpClient.post('http://localhost:8080/persons/register',this.signUp.value).subscribe({
+        next: () => {
+
+          this.router.navigate(["/"])
+        },
+        error: () => this._snackBar.open("Your authentification failed. Please retry with different credentials","Retry"),
+      }
+    )
+  }
 }
