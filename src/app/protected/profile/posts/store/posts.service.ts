599015c1d242e8a8d06e160797b3742dac40c129
Zakariae Moutaouakil <moutaouakilzakariae.cpgea@gmail.com>
2024-02-20 21:16:23 +0100

    Fixing API URL error

--- src/app/protected/profile/posts/store/posts.service.ts
+++ src/app/protected/profile/posts/store/posts.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Post} from "./posts-state";
 import {map} from "rxjs";
+import {environment} from "../../../../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
@@ -13,14 +14,14 @@ export class PostsService {
 
   getPosts(username: string, pageNumber: number) {
     return this.httpClient
-      .get<any>(`http://localhost:8080/posts/preview/${username}?pageNumber=${pageNumber}`)
+      .get<any>(environment.apiUrl + `posts/preview/${username}?pageNumber=${pageNumber}`)
       .pipe(
         map(response => response.content as Post[])
       )
   }
 
   getTotalPages(username: string) {
-    return this.httpClient.get<any>(`http://localhost:8080/posts/preview/${username}?pageNumber=0`)
+    return this.httpClient.get<any>(environment.apiUrl + `posts/preview/${username}?pageNumber=0`)
       .pipe(
         map(response => response.totalPages as number)
       )
